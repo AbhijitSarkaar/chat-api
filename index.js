822601e530fb1cb42d685bc0489ad74a3e37c975
Abhijit Sarkar <abhijitsarkar@Abhijits-MacBook-Air.local>
2019-07-04 02:43:50 +0530

    refactor

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ let app = express();
 let server = require('http').createServer(app);
 let io = require('socket.io')(server);
 
-//https://stackoverflow.com/questions/13745519/send-custom-data-along-with-handshakedata-in-socket-io
-
 io.on('connection', (socket) => {
     socket.on('client', (data) => {
         socket.broadcast.emit('server', data);
